4fd5eb85aba94ddc8fe2adca238519ec3941ea1d
guillenjs <jorgeguilln@gmail.com>
2020-06-26 11:58:50 -0400

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -6,7 +6,12 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+cult1 = Cult.new("Vamps", "NYC", 2020, "Vampires for life")
+cult2 = Cult.new("Catoholic", "Brooklyn", 2019, "MEOW")
+cult3 = Cult.new("Dogaholics", "BRONX", 2019, "Laugh")
 
+cult1.recruit_follower("Jorge")
+cult2.recruit_follower("Anh")
 binding.pry
 
 puts "Mwahahaha!" # just in case pry is buggy and exits
